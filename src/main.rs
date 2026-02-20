4495c93b3842d963bc870f00df737bd3e886dbae
Dmitry <andrewgamer63@gmail.com>
2026-02-20 19:19:58 +0300

    Args passing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 use crate::lexer::Lexer;
 use anyhow::{Context, Result};
-use std::fs::read_to_string;
+use std::{
+    env::{Args, args},
+    fs::read_to_string,
+};
 
 mod lexer;
 
 fn main() -> Result<()> {
-    let filename = "./urls.txt";
-    let contents = read_to_string(filename).context(format!("File: {}", filename))?;
+    let args: Vec<String> = args().collect();
+    if args.len() < 2 {
+        eprintln!("Filename was not provided!");
+        return Ok(());
+    }
+    let filename = &args[1];
+    let contents = read_to_string(&filename).context(format!("File: {}", filename))?;
     let mut lexer = Lexer::new();
     lexer.lex(&contents);
     println!("Found lexems:```");
